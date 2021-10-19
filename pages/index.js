85304ad45d61c48739a055e5e33022a37ce1bb2d
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-10-19 15:13:09 +0200

    render list of products

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import { Fragment } from "react";
+import { products } from "../products";
+
+const productList = products.map((product) => <li key={product.title}>{product.title}</li>);
+
 
 export default function Home() {
   return (
     <Fragment>
       <h1 className="text-7xl">Hoofdpagina</h1>
-      <Link href="/products">
-        Producten
-      </Link>
+      <Link href="/products">Producten</Link>
+      {productList}
+      {/* <p>{products[0].title}</p> */}
     </Fragment>
   );
 }
---
+++ products.js
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    "title": "Kracht zwemtraining voor gevorderden",
+    "price": "10",
+    "description":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare, augue auctor venenatis commodo, ante felis tristique lacus, a vehicula risus purus ac nisl. Curabitur vel rhoncus sapien, id ultricies enim. In hac habitasse platea dictumst. Curabitur orci urna, aliquet ac turpis sit amet, vestibulum mattis orci. Curabitur nec turpis aliquet, congue libero sed, porttitor sem. In fringilla, libero sit amet luctus gravida, elit eros aliquet lacus, nec vulputate ex lectus a lorem. Donec nunc nisi, tincidunt id lacus sit amet, aliquet maximus libero. Vivamus ante velit, viverra id metus sed, tincidunt mattis metus. In hac habitasse platea dictumst. Morbi commodo sagittis pellentesque. Pellentesque in pretium purus, vitae mattis massa. Mauris sit amet pulvinar magna, vitae tempor nisi. Duis a consequat ligula. Cras ex nibh, bibendum vitae urna congue, consectetur porta ante.",
+    "level": "gevorderden",
+    "tags": ["training", "kracht"],
+  },
+  {
+    "title": "Duur zwemtraining voor gevorderden",
+    "price": "20",
+    "description":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare, augue auctor venenatis commodo, ante felis tristique lacus, a vehicula risus purus ac nisl. Curabitur vel rhoncus sapien, id ultricies enim. In hac habitasse platea dictumst. Curabitur orci urna, aliquet ac turpis sit amet, vestibulum mattis orci. Curabitur nec turpis aliquet, congue libero sed, porttitor sem. In fringilla, libero sit amet luctus gravida, elit eros aliquet lacus, nec vulputate ex lectus a lorem. Donec nunc nisi, tincidunt id lacus sit amet, aliquet maximus libero. Vivamus ante velit, viverra id metus sed, tincidunt mattis metus. In hac habitasse platea dictumst. Morbi commodo sagittis pellentesque. Pellentesque in pretium purus, vitae mattis massa. Mauris sit amet pulvinar magna, vitae tempor nisi. Duis a consequat ligula. Cras ex nibh, bibendum vitae urna congue, consectetur porta ante.",
+    "level": "gevorderden",
+    "tags": ["training", "duur"],
+  },
+];
