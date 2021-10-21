ea3e6fab65feb5322ca59c18d7c4418ff5d443c1
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-10-21 13:02:09 +0200

    winkelwagenitems rendered

--- components/winkelwagen/WinkelwagenItem.js
+++ components/winkelwagen/WinkelwagenItem.js
@@ -1,10 +1,16 @@
-function WinkelwagenItem() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Image from "next/image";
+import { Fragment } from "react";
+
+function WinkelwagenItem(props) {
+  return (
+    <Fragment>
+      <li key={props.i}>
+        <Image src={props.images[0]} width={400} height={400} />
+        <h2>{props.title}</h2>
+        <h2>{props.price} euro</h2>
+      </li>
+    </Fragment>
+  );
 }
 
-
-export default WinkelwagenItem
+export default WinkelwagenItem;
--- pages/winkelwagen.js
+++ pages/winkelwagen.js
@@ -1,6 +1,5 @@
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import { useSelector } from "react-redux";
-import Image from "next/image";
 import WinkelwagenItem from "../components/winkelwagen/WinkelwagenItem";
 
 function winkelwagen() {
@@ -14,11 +13,16 @@ function winkelwagen() {
       </h1>
       {items.length ? (
         <div>
-          <div>
-            <Image src={items[0].images[0]} width={400} height={400} />
-            <h2>{items[0].title}</h2>
-            <h2>{items[0].price} euro</h2>
-          </div>
+          <ul>
+            {items.map((item, i) => (
+              <WinkelwagenItem
+                key={i}
+                title={item.title}
+                images={item.images}
+                price={item.price}
+              />
+            ))}
+          </ul>
           <div className="text-white max-w-md text-xs cursor-pointer bg-blue-700 rounded-full p-2 px-4">
             <div>
               <p className="hidden md:inline font-bold md:text-sm">Checkout</p>
