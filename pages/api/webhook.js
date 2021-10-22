3618778f83e51930c91738594d336f845f8d705c
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-10-22 12:52:01 +0200

    succesfully setup stripe webhook

---
+++ pages/api/webhook.js
@@ -0,0 +1,50 @@
+import { buffer } from "micro";
+
+// Establish connection to Stripe
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+console.log("established connection to stripe");
+
+const fulfillOrder = async (session) => {
+  return console.log("Fulfilling order", session);
+};
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const sig = req.headers["stripe-signature"];
+
+    console.log(req.headers);
+
+    let event;
+
+    // Verify that the EVENT posted came from stripe
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      console.log("ERROR", err.message);
+      return res.status(400).send(`Webhook error: ${err.message}`);
+    }
+
+    // Handle the checkout.session.completed event
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the order...
+      return fulfillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
+    }
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
