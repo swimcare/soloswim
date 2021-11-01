dd023947fa0a8e6d7590a8c50844d59814770ef8
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-11-01 14:43:56 +0100

    initial setup of flexbox navbar

--- components/main/Header.js
+++ components/main/Header.js
@@ -7,12 +7,18 @@ function Header() {
   const items = useSelector(selectItems);
 
   return (
-    <div className="w-full flex flex-row justify-end">
-      <div className="text-white text-xs cursor-pointer bg-blue-700 rounded-full p-2 px-4">
-        <div onClick={() => router.push("/winkelwagen")}>
-          <p className="hidden md:inline font-bold md:text-sm">
-            Winkelwagen ({items.length})
-          </p>
+    <div className="bg-white h-16 w-full drop-shadow-md">
+      <div className="flex flex-row justify-between max-w-7xl mx-auto h-full items-center">
+        <div className="">Logo</div>
+        <div>
+          <ul className="flex flex-row space-x-10">
+            <li>Zwemschema's</li>
+            <li>Over Soloswim</li>
+            <li>Contact</li>
+            <li onClick={() => router.push("/winkelwagen")}>
+              Winkelwagen ({items.length})
+            </li>
+          </ul>
         </div>
       </div>
     </div>
