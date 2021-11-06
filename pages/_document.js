957214b493c90434a53e0cc7e2f8cea8b313756c
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-11-06 09:53:36 +0100

    Update _document.js
    update font links

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="nl">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600;0,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;0,700;0,800;1,400&display=swap"
             rel="stylesheet"
           />
           <link
