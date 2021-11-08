067c70e526ed107867789a13805049f01f6acc8f
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-11-08 11:27:40 +0100

    Update index.js
    zwemschemas page started

--- pages/zwemschemas/index.js
+++ pages/zwemschemas/index.js
@@ -1,24 +1,32 @@
 import Link from "next/link";
-import ProductCards from "../../components/zwemschemas/ProductCards";
 
 import { getSortedProductsData } from "../../lib/products";
 
 function index({ allProductsData }) {
   return (
-    <div>
-      <h1 className="text-3xl mb-10">Zwemschema's</h1>
-      <Link href="/" passHref>
-        <a className="underline">Home</a>
-      </Link>
-      {/* {productList} */}
-      <ul className="grid grid-cols-3 z-0">
-        <ProductCards />
-      </ul>
-      <h1 className="text-3xl mb-10">Zwemschema's</h1>
+    <main>
+      <section>
+        <div className="px-5 sm:px-8 max-w-screen-xl mx-auto py-8 lg:py-20">
+          <div>
+            <h1 className="text-main font-lexend font-extrabold text-3xl lg:text-6xl my-2 lg:my-6">
+              De zwembundels van Soloswim
+            </h1>
+            <h2 className="text-navy-light1 font-lexend font-extrabold text-xl lg:text-4xl my-2 lg:my-4">
+              Ondertitel
+            </h2>
+            <p className="text-navy-light1 text-tiny leading-6">
+              Bij Soloswim vind je zwemtrainingen waarmee jij nog meer uit je
+              zwemmoment kunt halen. Zwem je graag lange afstanden, of hou je
+              meer van explosiviteit? Bekijk ons diverse aanbod en ga gauw aan
+              de slag met jouw eigen Soloswim bundel! Moeite met kiezen? Ga voor
+              de combo
+            </p>
+          </div>
+        </div>
+      </section>
 
       {/* Add this <section> tag below the existing <section> tag */}
       <section>
-        <h2 className="text-xl">Schema's</h2>
         <ul>
           {allProductsData.map(({ id, date, title }) => (
             <li key={id}>
@@ -29,7 +37,7 @@ function index({ allProductsData }) {
           ))}
         </ul>
       </section>
-    </div>
+    </main>
   );
 }
 
