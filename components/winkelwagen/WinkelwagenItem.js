5fe936f4fd193768ad660b7ef1576d9144a0d3b1
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-11-09 11:55:51 +0100

    made component for winkelwagen item

--- components/winkelwagen/WinkelwagenItem.js
+++ components/winkelwagen/WinkelwagenItem.js
@@ -1,16 +1,51 @@
 import Image from "next/image";
-import { Fragment } from "react";
+import { CheckIcon } from "@heroicons/react/solid";
 
 function WinkelwagenItem(props) {
   return (
-    <Fragment>
-      <li key={props.i}>
-        <Image src={props.images[0]} width={400} height={400} />
-        <h2>{props.title}</h2>
-        <h2>{props.level}</h2>
-        <h2>{props.price} euro</h2>
-      </li>
-    </Fragment>
+    <li key={props.i} className="border-grey-light5 border-b-2 pb-5 pt-6">
+      <div className="flex flex-row flex-wrap justify-between mx-4">
+        <div className="w-full">
+          <h2 className="font-semibold font-lexend md:text-lg text-base leading-5">
+            {props.title}
+          </h2>
+          <h3 className="text-xs md:text-sm my-1">{props.level}</h3>
+        </div>
+        <div className="w-32 my-2">
+          <Image src={props.images[0]} width={300} height={300} />
+        </div>
+        <div className="my-2">
+          <p className="font-lexend text-tiny md:text-lg font-semibold">
+            €{props.price}
+          </p>
+          <div className="flex flex-row justify-between text-2xl">
+            {/* todo add onclick event to buttons */}
+            <button role="button" className="hover:font-bold">
+              +
+            </button>
+            <button role="button" className="hover:font-bold">
+              -
+            </button>
+          </div>
+        </div>
+        <div className="w-full flex flex-row justify-between md:text-sm text-xs">
+          <div>
+            <p>
+              <span>
+                <CheckIcon className="w-4 h-4 mr-1 text-green-500 float-left stroke-current stroke-2" />
+              </span>
+              Op voorraad
+            </p>
+          </div>
+          <button
+            role="button"
+            className="text-main hover:underline font-semibold"
+          >
+            Verwijder
+          </button>
+        </div>
+      </div>
+    </li>
   );
 }
 
--- pages/winkelwagen.js
+++ pages/winkelwagen.js
@@ -39,125 +39,40 @@ function winkelwagen() {
     <main className="bg-grey-light4">
       <section className="px-5 sm:px-8 max-w-screen-xl mx-auto py-8 lg:py-20 text-navy-light1">
         <div>
-          <h1 className="font-lexend text-2xl text-center font-extrabold text-main">
+          <h1 className="font-lexend text-2xl md:text-4xl lg:text-6xl text-center font-extrabold text-main">
             Winkelwagen
           </h1>
         </div>
-        <div className="bg-white w-full rounded-3xl px-4 my-8">
+        <div className="bg-white w-full rounded-3xl px-4 my-8 lg:my-16">
           {items.length ? (
-            <div>
+            <div className="max-w-4xl mx-auto">
               <ul>
-                <li className="border-grey-light5 border-b-2 pb-5 pt-6">
-                  <div className="flex flex-row flex-wrap justify-between mx-4">
-                    <div className="w-full">
-                      <h2 className="font-semibold font-lexend text-lg leading-5">
-                        Borstcrawl kracht zwemtraining
-                      </h2>
-                      <h3 className="text-xs my-1">Beginners</h3>
-                    </div>
-                    <div className="w-32 my-2">
-                      <Image
-                        src="/images/zwemschemas/1-1.jpg"
-                        width={300}
-                        height={300}
-                      />
-                    </div>
-                    <div className="my-2">
-                      <p className="font-lexend text-tiny font-semibold">
-                        €11,99
-                      </p>
-                      <div className="flex flex-row justify-between text-2xl">
-                        {/* todo add onclick event to buttons */}
-                        <button role="button" className="hover:font-bold">
-                          +
-                        </button>
-                        <button role="button" className="hover:font-bold">
-                          -
-                        </button>
-                      </div>
-                    </div>
-                    <div className="w-full flex flex-row justify-between text-xs">
-                      <div>
-                        <p>
-                          <span>
-                            <CheckIcon className="w-4 h-4 mr-1 text-green-500 float-left stroke-current stroke-2" />
-                          </span>
-                          Op voorraad
-                        </p>
-                      </div>
-                      <button
-                        role="button"
-                        className="text-main hover:underline font-semibold"
-                      >
-                        Verwijder
-                      </button>
-                    </div>
-                  </div>
-                </li>
-                <li className="border-grey-light5 border-b-2 pb-5 pt-6">
-                  <div className="flex flex-row flex-wrap justify-between mx-4">
-                    <div className="w-full">
-                      <h2 className="font-semibold font-lexend text-lg leading-5">
-                        Borstcrawl kracht zwemtraining
-                      </h2>
-                      <h3 className="text-xs my-1">Beginners</h3>
-                    </div>
-                    <div className="w-32 my-2">
-                      <Image
-                        src="/images/zwemschemas/1-1.jpg"
-                        width={300}
-                        height={300}
-                      />
-                    </div>
-                    <div className="my-2">
-                      <p className="font-lexend text-tiny font-semibold">
-                        €11,99
-                      </p>
-                      <div className="flex flex-row justify-between text-2xl">
-                        {/* todo add onclick event to buttons */}
-                        <button role="button" className="hover:font-bold">
-                          +
-                        </button>
-                        <button role="button" className="hover:font-bold">
-                          -
-                        </button>
-                      </div>
-                    </div>
-                    <div className="w-full flex flex-row justify-between text-xs">
-                      <div>
-                        <p>
-                          <span>
-                            <CheckIcon className="w-4 h-4 mr-1 text-green-500 float-left stroke-current stroke-2" />
-                          </span>
-                          Op voorraad
-                        </p>
-                      </div>
-                      <button
-                        role="button"
-                        className="text-main hover:underline font-semibold"
-                      >
-                        Verwijder
-                      </button>
-                    </div>
-                  </div>
-                </li>
+                {items.map((item, i) => (
+                  <WinkelwagenItem
+                    key={i}
+                    title={item.title}
+                    images={item.images}
+                    price={item.price}
+                    level={item.level}
+                  />
+                ))}
               </ul>
               <div className="py-4">
-                <div className="flex flex-row justify-between my-3 font-semibold font-lexend text-tiny">
+                <div className="flex flex-row justify-between my-3 font-semibold font-lexend text-tiny md:text-lg">
                   <p>
                     Subtotaal
                     <span className="text-xs font-normal"> (incl. BTW)</span>
                   </p>
                   <p>€11,99</p>
                 </div>
-                <div className="flex flex-row justify-between my-3 font-semibold font-lexend text-tiny">
+                <div className="flex flex-row justify-between my-3 font-semibold font-lexend text-tiny md:text-lg">
                   <p>
                     Verzendkosten
                     <span className="text-xs font-normal"> (incl. BTW)</span>
                   </p>
                   <p>€3,95</p>
                 </div>
-                <div className="flex flex-row justify-between my-3 font-semibold font-lexend text-tiny">
+                <div className="flex flex-row justify-between my-3 font-semibold font-lexend text-tiny md:text-lg">
                   <p>
                     Totaal
                     <span className="text-xs font-normal"> (incl. BTW)</span>
@@ -173,6 +88,11 @@ function winkelwagen() {
                 >
                   Afrekenen
                 </button>
+                <div className="text-center py-2">
+                  <Link href="/producten">
+                    <a className="hover:underline">Verder winkelen</a>
+                  </Link>
+                </div>
               </div>
             </div>
           ) : (
@@ -203,7 +123,7 @@ function winkelwagen() {
         </div>
       </section>
 
-      <h1 className="text-3xl">
+      {/* <h1 className="text-3xl">
         {items.length === 0 ? "Je Winkelwagen is leeg." : "Winkelwagen"}
       </h1>
       <p>Totaal: {total}</p>
@@ -230,7 +150,7 @@ function winkelwagen() {
         </div>
       ) : (
         <p>Geen producten</p>
-      )}
+      )} */}
 
       {/* {items.map((item, i) => (
         <WinkelwagenItem
