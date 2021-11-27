b40829169d6b07045ed1e48c136e0240afc590d3
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-11-27 18:36:14 +0100

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+function contact() {
+  return (
+    <div className="h-screen text-center">
+      <div className="mt-32">
+        <h1>Hier komt een contactpagina met formulier</h1>
+      </div>
+    </div>
+  );
+}
+
+export default contact;
