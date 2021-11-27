0ad3b660b932df8fca79b2551bc48273cd180379
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-11-27 09:20:46 +0100

    Create over-ons.js

---
+++ pages/over-ons.js
@@ -0,0 +1,18 @@
+function overOns() {
+    return (
+        <section>
+        <div className="px-5 sm:px-8 max-w-screen-xl mx-auto py-20 lg:py-32">
+          <div>
+            <h2 className="text-main font-lexend font-extrabold text-3xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+              Herbruikbare zwemschema's
+            </h2>
+            <h3 className="text-navy-light1 font-lexend font-bold md:font-extrabold text-lg xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl my-2 md:my-5">
+              Voor tijdens het banenzwemmen
+            </h3>
+          </div>
+        </div>
+        </section>
+    )
+}
+
+export default overOns
