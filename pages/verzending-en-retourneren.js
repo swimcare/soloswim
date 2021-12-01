0f80ad7413d73f1e85793bbb39fb28b3af8edd2a
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-12-01 10:35:34 +0100

    Update verzending-en-retourneren.js

--- pages/verzending-en-retourneren.js
+++ pages/verzending-en-retourneren.js
@@ -14,9 +14,34 @@ function verzendingEnRetourneren() {
           <h3 className="text-main text-lg font-lexend font-bold">
             Artikel 2 - Identiteit van de ondernemer
           </h3> */}
-        <h3 className="text-main text-lg font-lexend font-bold">
+        <h3 className="text-main text-2xl font-lexend font-bold my-10">
           Pagina nog onder constructie
         </h3>
+        <h3 className="text-main text-lg font-lexend font-bold">
+          Verzendkosten en levertijd
+        </h3>
+        <p className="my-5">
+          Verzendkosten: 3,95 euro. Levertijd: 1-2 werkdagen.
+        </p>
+        <h3 className="text-main text-lg font-lexend font-bold">
+          Retourneren
+        </h3>
+        <p className="my-5">
+          U heeft het recht uw bestelling tot 14 dagen na ontvangst zonder
+          opgave van reden te annuleren. U heeft na annulering nogmaals 14 dagen
+          om uw product retour te sturen. U krijgt dan het volledige orderbedrag
+          inclusief verzendkosten gecrediteerd. Enkel de kosten voor retour van
+          u thuis naar de webwinkel zijn voor eigen rekening. Deze kosten
+          bedragen circa 4 euro per brievenbuszending, raadpleeg voor de exacte tarieven de
+          website van uw vervoerder. Indien u gebruik maakt van uw
+          herroepingsrecht, zal het product met alle geleverde toebehoren en –
+          indien redelijkerwijze mogelijk – in de originele staat en verpakking
+          aan Soloswim geretourneerd worden. Om gebruik te maken van dit
+          recht kunt u contact met ons opnemen via info@soloswim.nl. Wij
+          zullen vervolgens het verschuldigde orderbedrag binnen 14 dagen na
+          aanmelding van uw retour terugstorten mits het product reeds in goede
+          orde retour ontvangen is.
+        </p>
       </div>
     </section>
   );
