eb466ed64d762b696c7c8b4af8b2e2d9d1598726
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-12-04 18:34:27 +0100

    Update verzending-en-retourneren.js

--- pages/verzending-en-retourneren.js
+++ pages/verzending-en-retourneren.js
@@ -8,26 +8,17 @@ function verzendingEnRetourneren() {
       <main>
         <section className="bg-grey-light4">
           <div className="px-5 sm:px-8 mx-auto max-w-3xl py-20 lg:py-32">
-            {/* <h1 className="text-main text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-lexend font-extrabold my-5">
-            Algemene Voorwaarden
-          </h1>
-          <h2 className="text-2xl font-lexend font-extrabold my-5 text-navy-light1">
-            Inhoudsopgave
-          </h2>
-          <h3 className="text-main text-lg font-lexend font-bold">
-            Artikel 1 - Definities
-          </h3>
-          <h3 className="text-main text-lg font-lexend font-bold">
-            Artikel 2 - Identiteit van de ondernemer
-          </h3> */}
-            <h3 className="text-main text-2xl font-lexend font-bold my-10">
-              Pagina nog onder constructie
-            </h3>
+          <h1 className="text-main text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-lexend font-extrabold my-5">
+              Verzending &amp; retourneren
+            </h1>
             <h3 className="text-main text-lg font-lexend font-bold">
-              Verzendkosten en levertijd
+              Verzending
             </h3>
             <p className="my-5">
-              Verzendkosten: 3,95 euro. Levertijd: 1-2 werkdagen.
+              Verzendkosten: <strong>3,95 euro</strong>.
+            </p>
+            <p className="my-5">
+              Standaard levertijd (tenzij anders aangegeven): <strong>1-2 werkdagen</strong>.
             </p>
             <h3 className="text-main text-lg font-lexend font-bold">
               Retourneren
@@ -37,14 +28,14 @@ function verzendingEnRetourneren() {
               opgave van reden te annuleren. U heeft na annulering nogmaals 14
               dagen om uw product retour te sturen. U krijgt dan het volledige
               orderbedrag inclusief verzendkosten gecrediteerd. Enkel de kosten
-              voor retour van u thuis naar de webwinkel zijn voor eigen
+              voor retour van u thuis naar Soloswim zijn voor eigen
               rekening. Deze kosten bedragen circa 4 euro per brievenbuszending,
               raadpleeg voor de exacte tarieven de website van uw vervoerder.
               Indien u gebruik maakt van uw herroepingsrecht, zal het product
               met alle geleverde toebehoren en – indien redelijkerwijze mogelijk
               – in de originele staat en verpakking aan Soloswim geretourneerd
               worden. Om gebruik te maken van dit recht kunt u contact met ons
-              opnemen via info@soloswim.nl. Wij zullen vervolgens het
+              opnemen via <a href="mailto:info@soloswim.nl" className="hover:text-main underline"> info@soloswim.nl</a>. Vermeld hierbij uw ordernummer (deze kunt u terug vinden in de orderbevestiging welke u per mail heeft ontvangen). Wij zullen vervolgens het
               verschuldigde orderbedrag binnen 14 dagen na aanmelding van uw
               retour terugstorten mits het product reeds in goede orde retour
               ontvangen is.
