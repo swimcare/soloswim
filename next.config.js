b3fd7b69c4ff63779ae8d16dc1f6f81f89064267
Laurens <36268730+Sneruall@users.noreply.github.com>
2022-03-06 13:55:09 +0100

    Update next.config.js
    Borstcrawl gevorderden duur redirects

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ module.exports = {
   // REDIRECT URLS FOR QR CODES ON TRAINING SCHEDULES
   async redirects() {
     return [
+      //Review link Google
       {
         source: "/review",
         destination: "https://g.page/r/CRxat-X_lZvOEAg/review",
@@ -70,6 +71,57 @@ module.exports = {
       {
         source: "/BCK10G1",
         destination: "https://vimeo.com/681900439/40a8a8a1d5",
+        permanent: true,
+      },
+      // Borstcrawl Gevorderden Duur Editie 1
+      {
+        source: "/BCD1G1",
+        destination: "https://vimeo.com/684925536/ec298872d1",
+        permanent: true,
+      },
+      {
+        source: "/BCD2G1",
+        destination: "https://vimeo.com/684925707/a4259c5174",
+        permanent: true,
+      },
+      {
+        source: "/BCD3G1",
+        destination: "https://vimeo.com/684925805/b0e901698f",
+        permanent: true,
+      },
+      {
+        source: "/BCD4G1",
+        destination: "https://vimeo.com/684925975/709c9320f1",
+        permanent: true,
+      },
+      {
+        source: "/BCD5G1",
+        destination: "https://vimeo.com/684926130/92e5f12719",
+        permanent: true,
+      },
+      {
+        source: "/BCD6G1",
+        destination: "https://vimeo.com/684743812/92ca0eecce",
+        permanent: true,
+      },
+      {
+        source: "/BCD7G1",
+        destination: "https://vimeo.com/684744010/c6543c1b07",
+        permanent: true,
+      },
+      {
+        source: "/BCD8G1",
+        destination: "https://vimeo.com/684744198/6adcc685ef",
+        permanent: true,
+      },
+      {
+        source: "/BCD9G1",
+        destination: "https://vimeo.com/684744393/b5a1a464e6",
+        permanent: true,
+      },
+      {
+        source: "/BCD10G1",
+        destination: "https://vimeo.com/684744603/328f1deb15",
         permanent: true,
       },
     ];
