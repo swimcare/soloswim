a93045fdc993114baff0e40054abd0a226fb7fe5
Sneruall <laurens@vr-house.nl>
2024-03-12 16:31:22 +0100

    adding title and description to metadata in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Soloswim | Waterproof zwemschema's",
+  description:
+    "Waterproof zwemschema's om zelf te volgen vanuit het zwembad. ✓ Borstcrawl zwemschema's ✓ Techniek-, kracht- en duurtrainingen ✓ Alle niveau's ✓ Snelle levering",
+};
+
 export default function RootLayout({
   children,
 }: {
