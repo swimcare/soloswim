d946847e0a7d84eb114aa1f1f0a6bad369b013ac
Sneruall <laurens@vr-house.nl>
2024-03-12 16:40:09 +0100

    migrated opengraph to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,22 @@ export const metadata: Metadata = {
   title: "Soloswim | Waterproof zwemschema's",
   description:
     "Waterproof zwemschema's om zelf te volgen vanuit het zwembad. ✓ Borstcrawl zwemschema's ✓ Techniek-, kracht- en duurtrainingen ✓ Alle niveau's ✓ Snelle levering",
+  openGraph: {
+    url: "https://www.soloswim.nl",
+    title: "Soloswim | Waterproof zwemschema's",
+    description:
+      "Waterproof zwemschema's om zelf te volgen vanuit het zwembad. ✓ Borstcrawl zwemschema's ✓ Techniek-, kracht- en duurtrainingen ✓ Alle niveau's ✓ Snelle levering",
+    locale: "nl_NL",
+    siteName: "Soloswim | Waterproof zwemschema's",
+    images: [
+      {
+        url: "/images/home/header-OG.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Soloswim",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
