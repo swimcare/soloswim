5a38c6a455b033c46d43d0e9d7cfffd1e15ffecb
Sneruall <laurens@vr-house.nl>
2024-03-15 08:08:30 +0100

    update cartmodal to make it center

--- components/products/CartModal.tsx
+++ components/products/CartModal.tsx
@@ -42,64 +42,62 @@ function Modal({ productData }) {
                 role="dialogue"
                 tabIndex={-1}
               >
-                <div className="animate-modal relative flex flex-col bg-white pointer-events-auto rounded-2xl h-full">
-                  <div className="max-w-2xl">
-                    <div className="flex justify-between border-b border-gray-300 bg-main rounded-t-2xl">
-                      <div className="p-4 text-xl font-lexend font-semibold text-white">
-                        <h1>Het product is toegevoegd aan je winkelwagen</h1>
-                      </div>
-                      <Link href={pathname || ""}>
-                        <button type="button" className="self-start m-3">
-                          <XIcon className="h-6 w-6 text-white" />
-                        </button>
-                      </Link>
+                <div className="animate-modal relative flex flex-col bg-white pointer-events-auto rounded-2xl h-full max-w-2xl mx-auto">
+                  <div className="flex justify-between border-b border-gray-300 bg-main rounded-t-2xl">
+                    <div className="p-4 text-xl font-lexend font-semibold text-white">
+                      <h1>Het product is toegevoegd aan je winkelwagen</h1>
                     </div>
-                    <div className="flex-shrink flex-grow p-4">
-                      <div className="flex flex-row gap-5 my-5">
-                        <div className="w-44">
-                          <Image
-                            alt={productData.title}
-                            src={
-                              productData.type && productData.winkelwagen_images
-                                ? productData.winkelwagen_images[typeNumber()]
-                                : productData.images[0]
-                            }
-                            width={300}
-                            height={300}
-                            style={{
-                              maxWidth: "100%",
-                              height: "auto",
-                            }}
-                          />
-                        </div>
-                        <div>
-                          <h2 className="font-semibold font-lexend md:text-lg text-base leading-5">
-                            <p>{productData.title}</p>
-                          </h2>
-                          <h3 className="text-xs md:text-sm my-1">
-                            <p>{productData.type}</p>
-                          </h3>
-                        </div>
+                    <Link href={pathname || ""}>
+                      <button type="button" className="self-start m-3">
+                        <XIcon className="h-6 w-6 text-white" />
+                      </button>
+                    </Link>
+                  </div>
+                  <div className="flex-shrink flex-grow p-4">
+                    <div className="flex flex-row gap-5 my-5">
+                      <div className="w-44">
+                        <Image
+                          alt={productData.title}
+                          src={
+                            productData.type && productData.winkelwagen_images
+                              ? productData.winkelwagen_images[typeNumber()]
+                              : productData.images[0]
+                          }
+                          width={300}
+                          height={300}
+                          style={{
+                            maxWidth: "100%",
+                            height: "auto",
+                          }}
+                        />
+                      </div>
+                      <div>
+                        <h2 className="font-semibold font-lexend md:text-lg text-base leading-5">
+                          <p>{productData.title}</p>
+                        </h2>
+                        <h3 className="text-xs md:text-sm my-1">
+                          <p>{productData.type}</p>
+                        </h3>
                       </div>
                     </div>
-                    <div className="flex flex-wrap items-center justify-center p-3 xs:gap-5 border-t border-gray-300">
-                      <Link href={pathname || ""}>
-                        <button
-                          type="button"
-                          className="text-black font-medium hover:cursor-pointer hover:underline"
-                        >
-                          Verder Winkelen
-                        </button>
-                      </Link>
-                      <Link href={"/winkelwagen" || ""}>
-                        <button
-                          type="button"
-                          className="text-white focus:outline-none m-1.5 rounded-full px-6 py-2 bg-main hover:bg-white hover:text-main border-2 border-main"
-                        >
-                          Naar winkelwagen
-                        </button>
-                      </Link>
-                    </div>
+                  </div>
+                  <div className="flex flex-wrap items-center justify-center p-3 xs:gap-5 border-t border-gray-300">
+                    <Link href={pathname || ""}>
+                      <button
+                        type="button"
+                        className="text-black font-medium hover:cursor-pointer hover:underline"
+                      >
+                        Verder Winkelen
+                      </button>
+                    </Link>
+                    <Link href={"/winkelwagen" || ""}>
+                      <button
+                        type="button"
+                        className="text-white focus:outline-none m-1.5 rounded-full px-6 py-2 bg-main hover:bg-white hover:text-main border-2 border-main"
+                      >
+                        Naar winkelwagen
+                      </button>
+                    </Link>
                   </div>
                 </div>
               </div>
