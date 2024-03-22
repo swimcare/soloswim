0ced283132fe385a611446822b9c9b61d267bd1a
Sneruall <laurens@vr-house.nl>
2024-03-22 16:20:44 +0100

    fix animation on mobile method

--- components/framer/SlideInFromSide.tsx
+++ components/framer/SlideInFromSide.tsx
@@ -5,7 +5,7 @@ function SlideInFromSide({
   children,
   duration = 0.8,
   delay = 0,
-  initialX = "-700px",
+  initialX = "-wscreen",
 }: {
   children: React.ReactNode;
   duration: number;
@@ -14,8 +14,27 @@ function SlideInFromSide({
 }) {
   return (
     <motion.div
-      initial={{ x: initialX }}
-      // animate={{
+      initial="initialState"
+      animate="animateState"
+      exit="exitState"
+      transition={{
+        type: "tween",
+        duration: duration,
+        ease: [0.3, 0.4, 0.5, 0.95],
+        delay: delay,
+      }}
+      variants={{
+        initialState: {
+          x: "-100vw",
+        },
+        animateState: {
+          x: 0,
+        },
+        exitState: {
+          x: "100vw",
+        },
+      }}
+      // whileInView={{
       //   x: 0,
       //   transition: {
       //     duration: duration,
@@ -23,15 +42,7 @@ function SlideInFromSide({
       //     delay: delay,
       //   },
       // }}
-      whileInView={{
-        x: 0,
-        transition: {
-          duration: duration,
-          ease: [0.3, 0.4, 0.5, 0.95],
-          delay: delay,
-        },
-      }}
-      viewport={{ once: true }}
+      // viewport={{ once: true }}
     >
       {children}
     </motion.div>
