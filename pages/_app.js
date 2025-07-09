30eec302b32b7d5e4f61091cf4012635227b3075
Laurens Christiaan van Roomen <l.c.vanroomen@gmail.com>
2025-07-09 07:39:58 +0200

    add chatwoot

--- pages/_app.js
+++ pages/_app.js
@@ -4,46 +4,69 @@ import { store } from "../app/store";
 import Header from "../components/main/Header";
 import Footer from "../components/main/Footer";
 import "../styles/globals.css";
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as ga from '../lib/ga'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as ga from "../lib/ga";
 import { DefaultSeo } from "next-seo";
 import ScrollToTop from "../components/general/ScrollToTop";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
+      ga.pageview(url);
+    };
     //When the component is mounted, subscribe to router changes
     //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on("routeChangeComplete", handleRouteChange);
 
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <Provider store={store}>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d,t) {
+                var BASE_URL="https://chat.swimcare.be";
+                var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                g.src=BASE_URL+"/packs/js/sdk.js";
+                g.defer = true;
+                g.async = true;
+                s.parentNode.insertBefore(g,s);
+                g.onload=function(){
+                  window.chatwootSDK.run({
+                    websiteToken: 'WWg5SwD9hCS2z34iBYGzziwv',
+                    baseUrl: BASE_URL
+                  })
+                }
+              })(document,"script");
+            `,
+          }}
+        />
+      </Head>
       <DefaultSeo
-         title="SoloSwim | Waterproof zwemschema's"
-         description="Waterproof zwemschema's om zelf te volgen vanuit het zwembad. ✓ Borstcrawl zwemschema's ✓ Techniek-, kracht- en duurtrainingen ✓ Alle niveau's ✓ Snelle levering"
-         additionalLinkTags={[
-           {
-             rel: "icon",
-             href: "/images/favicons/favicon.ico",
-           },
-           {
-             rel: "apple-touch-icon",
-             href: "/images/favicons/apple-touch-icon.png",
-           },
-         ]}
-         openGraph={{
+        title="SoloSwim | Waterproof zwemschema's"
+        description="Waterproof zwemschema's om zelf te volgen vanuit het zwembad. ✓ Borstcrawl zwemschema's ✓ Techniek-, kracht- en duurtrainingen ✓ Alle niveau's ✓ Snelle levering"
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "/images/favicons/favicon.ico",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/images/favicons/apple-touch-icon.png",
+          },
+        ]}
+        openGraph={{
           type: "website",
           url: "https://www.soloswim.be",
           title: "SoloSwim | Waterproof zwemschema's",
